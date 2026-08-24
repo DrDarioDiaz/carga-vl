1adf2a86b4d2e9f394ae38615c97ce866cadb3b3
Darío Ezequiel Díaz <105292702+DrDarioDiaz@users.noreply.github.com>
2026-08-24 11:54:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    (vl-v1 -> vl-v2 -> vl-v3...). Eso obliga a los equipos a descargar la
    versión nueva la próxima vez que tengan conexión. */
 
-var VERSION = 'vl-v3';
+var VERSION = 'vl-v5';
 var ASSETS = [
   './',
   './index.html',
